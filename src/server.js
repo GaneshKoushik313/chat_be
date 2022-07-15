4e8b86768ab0a04a4fe7013271898369921bc7d4
Ganesh Koushik <ganeshkoushik313@gmail.com>
2022-07-15 12:59:57 +0530

    changes in server.js

--- src/server.js
+++ src/server.js
@@ -59,7 +59,7 @@ io.on('connection', (socket) => {
   	});
 })
 
-server.listen(5000, () => {
+server.listen(process.env.PORT || 5000, () => {
   console.log(`Server is now running on http://localhost:5000`)
   console.log(`Websocket is now running on ws://localhost:5000/<websocket-path>`)
 })
